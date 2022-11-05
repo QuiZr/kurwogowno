b896d8d52c33bd9a4840a4a4f24851aed40359c3
Krzysztof Osuch <krzysiekos@gmail.pl>
2022-11-05 15:04:11 +0100

    szit jednostki

--- kurwogowno/Player.cs
+++ kurwogowno/Player.cs
@@ -6,6 +6,7 @@ using static Raylib_cs.KeyboardKey;
 
 namespace HelloWorld {
   class Player: Actor {
+    private Boolean antiGravity = false;
     private Vector3 speedVector;
     public Player() {
       this.name = "player";
@@ -16,39 +17,56 @@ namespace HelloWorld {
 
       this.rotationAxis = new Vector3(0,0,0);
       if (IsKeyDown(KEY_UP)) {
-        this.rotationAngle = 21f;
+        this.rotationAngle = 45f;
         rotationAxis.X = 1;
       } else if (IsKeyDown(KEY_DOWN)) {
-        this.rotationAngle = 21f;
+        this.rotationAngle = 45f;
         rotationAxis.X = -1;
       }
       if (IsKeyDown(KEY_LEFT)) {
-        this.rotationAngle = 21f;
+        this.rotationAngle = 45f;
         rotationAxis.Y = 1;
       } else if (IsKeyDown(KEY_RIGHT)) {
-        this.rotationAngle = 21f;
+        this.rotationAngle = 45f;
         rotationAxis.Y = -1;
       }
-      var rotation = Quaternion.CreateFromAxisAngle(Vector3.Normalize(rotationAxis), rotationAngle );
-      Console.WriteLine(rotation);
-      var gravityVector = new Vector3(0,0,-dt*0.98f);
+      var rotation = Quaternion.CreateFromAxisAngle(Vector3.Normalize(rotationAxis), deg2rad(rotationAngle));
+      var gravityVector = new Vector3(0,0,-0.98f);
       var thrustVector = new Vector3(0,0,0);
-      if (IsKeyDown(KEY_SPACE)) {
-        thrustVector += new Vector3(0,0,dt*1.2f);
-        if (rotationAxis.LengthSquared() > 0) {
-          Console.WriteLine($"before, x: {thrustVector.X}, y: {thrustVector.Y}  z: {thrustVector.Z}");
-          thrustVector = Vector3.Transform(thrustVector, rotation);
-          Console.WriteLine($"after, x: {thrustVector.X}, y: {thrustVector.Y}  z: {thrustVector.Z}");
-        }
+      if (IsKeyDown(KEY_A)) {
+        thrustVector += new Vector3(0,0,1.4f);
+      } else if (IsKeyDown(KEY_Z)) {
+        thrustVector += new Vector3(0,0,-1.4f);
       }
-      speedVector += gravityVector;
-      speedVector += thrustVector;
+      if (rotationAxis.LengthSquared() > 0) {
+        thrustVector = Vector3.Transform(thrustVector, rotation);
+      }
+      if (thrustVector.LengthSquared() > 0) {
+        Console.WriteLine($"thrust: x: {thrustVector.X}, y: {thrustVector.Y}  z: {thrustVector.Z}");
+        speedVector += thrustVector*dt;
+      }
+      if (speedVector.LengthSquared() > 0) {
+        Console.WriteLine($"speed: x: {speedVector.X}, y: {speedVector.Y}  z: {speedVector.Z}");
+      }
 
-      var friction = speedVector*-1f*speedVector.LengthSquared();
+      if (IsKeyPressed(KEY_G)) {
+        this.antiGravity = !this.antiGravity;
+      }
+      if (!antiGravity) {
+        speedVector += gravityVector*dt;
+      }
+      var friction = speedVector*-.01f;
+      if (friction.LengthSquared() > 0) {
+        Console.WriteLine($"friction: x: {friction.X}, y: {friction.Y}  z: {friction.Z}");
+      }
       speedVector += friction;
       this.move(this.speedVector);
 
 
     }
+    public static float deg2rad(float deg) {
+      return (deg * (float)Math.PI)/180f; 
+    }
   } 
+
 }
